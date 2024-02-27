e5b89609d610ba188302e3ac4c7f123519c68286
rhaskia <rhaskiagamedev@gmail.com>
2024-02-27 17:06:59 +1300

    Switch to Signal Sync

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod input;
 mod renderer;
 mod terminal;
 
+use std::ops::Add;
 use app::App;
 use dioxus::prelude::*;
 
--- src/renderer/terminal.rs
+++ src/renderer/terminal.rs
@@ -14,12 +14,11 @@ pub struct FontInfo {
 // TODO: split this up for the use of multiple ptys per terminal
 #[component]
 pub fn TerminalApp(pair: Signal<PtyPair>) -> Element {
-    let mut terminal = use_signal(|| Terminal::setup().unwrap());
+    let mut terminal = use_signal_sync(|| Terminal::setup().unwrap());
     let mut screen = use_signal(|| TerminalRenderer::new());
+    let mut actions = use_signal_sync(|| Vec::new());
     
     let child = pair.write().slave.spawn_command(CommandBuilder::new("bash")).ok()?;
-
-    let mut writer = use_signal(|| pair.write().master.take_writer().unwrap());
 
     let input = use_signal(|| InputManager::new());
     let font_size = use_signal(|| 14);
@@ -50,15 +49,19 @@ pub fn TerminalApp(pair: Signal<PtyPair>) -> Element {
         //await dioxus.recv();
     "#,
     );
-
+    
+    // Writer future
     use_future(move || async move {
+        let mut writer = pair.write().master.take_writer().unwrap();
+        
         loop {
             let key = key_press.recv().await.unwrap();
+            println!("{actions:?}");
 
             match input.read().handle_key(key) {
-                Input::String(text) => writer.write().write_all(text.as_bytes()).unwrap(),
+                Input::String(text) => writer.write_all(text.as_bytes()).unwrap(),
                 Input::Control(c) => match c.as_str() {
-                    "c" => writer.write().write_all(b"\x03").unwrap(),
+                    "c" => writer.write_all(b"\x03").unwrap(),
                     _ => {}
                 },
                 _ => {}
@@ -70,16 +73,16 @@ pub fn TerminalApp(pair: Signal<PtyPair>) -> Element {
     use_future(move || async move {
         let mut reader = pair.write().master.try_clone_reader().unwrap();
 
-        tokio::spawn(async move {
+        std::thread::spawn(move || {
             let mut parser = termwiz::escape::parser::Parser::new();
             let mut buffer = [0u8; 1]; // Buffer to hold a single character
 
             loop {
                 let read = reader.read(&mut buffer);
-
+                
                 match read {
                     Ok(_) => {
-                        parser.parse(&buffer, |a| terminal.write().handle_action(a));
+                        parser.parse(&buffer, |a| actions.push(a)); // terminal.write().handle_action(a));
                     }
                     Err(err) => {
                         eprintln!("Error reading from Read object: {}", err);
@@ -95,13 +98,19 @@ pub fn TerminalApp(pair: Signal<PtyPair>) -> Element {
             script {
                 src: "/js/textsize.js"
             }
-            for l in terminal.read().get_cells() {
-                pre {
-                    for cell in l {
-                        CellSpan { cell: cell.clone() }
-                    }
-                }
+            pre {
+                "hi"
             }
+            // pre {
+            //     "{actions.read():?}"
+            // }
+            // for l in terminal.read().get_cells() {
+            //     pre {
+            //         for cell in l {
+            //             CellSpan { cell: cell.clone() }
+            //         }
+            //     }
+            // }
         }
     }
 }
