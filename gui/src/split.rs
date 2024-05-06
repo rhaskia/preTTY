708c2ffd0d037e1c999ffa1298415799637cf52b
rhaskia <rhaskiagamedev@gmail.com>
2024-05-06 17:28:00 +1200

    feat: start tabs

--- gui/src/split.rs
+++ gui/src/split.rs
@@ -3,12 +3,35 @@ use term::pty::PseudoTerminalSystem;
 
 use super::terminal::TerminalApp;
 
+#[derive(Clone, PartialEq)]
+pub struct Tab {
+    pub index: usize,
+    pub name: String,
+}
+
+impl Tab {
+    pub fn new(idx: usize) -> Self {
+        Tab {
+            index: idx,
+            name: format!("terminal {idx}")
+        }
+    }
+}
+
 #[component]
 pub fn TerminalSplit(tabs: bool) -> Element {
     // Set up vector arrangement
     let pty_system = use_signal(|| PseudoTerminalSystem::setup());
+    let tabs = use_signal(|| vec![Tab::new(0)]);
 
     rsx! {
-        TerminalApp { index: 0, pty_system },
+        div {
+            for tab in tabs.read().iter() {
+                span { "{tab.name}" }
+            }
+        }
+        for tab in tabs.read().iter() {
+            TerminalApp { tab: tab.clone(), pty_system }
+        }
     }
 }
