baf2105bf11dbaec6c625c83024dd2a84b7d2bf5
rhaskia <rhaskiagamedev@gmail.com>
2024-07-06 18:54:13 +1200

    more macros work

--- gui/src/menu.rs
+++ gui/src/menu.rs
@@ -3,10 +3,12 @@ mod keybinding;
 mod form;
 use keybinding::Keybind;
 use dioxus::prelude::*;
-use crate::CONFIG;
+use crate::{CONFIG};
+use macros::Form;
 use serde::Serialize;
+//use ::create_form;
 
-#[derive(Serialize)]
+#[derive(Serialize, Form)]
 struct Example {
     pub amount: i64,
 }
@@ -15,8 +17,7 @@ struct Example {
 pub fn Menu(active: bool) -> Element {
     // Temporary config
     let config = use_signal(|| CONFIG.cloned());
-    let value = Example { amount: 34 };
-    println!("{:?}", form::create_form(&value));
+    let value = use_signal(|| 87);
 
     rsx! {
         div {
@@ -31,6 +32,7 @@ pub fn Menu(active: bool) -> Element {
                   h2 { "Settings" }, 
                 }
                 div { "Font Size" input { r#type: "number", value: config().font_size } }
+                TestForm { value },
 
                 for i in 0..config().keybinds.len() {
                     Keybind { index: i, config }
