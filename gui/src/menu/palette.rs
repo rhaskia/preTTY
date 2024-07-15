08144a3fe417dd2f4cadf4b6baadb1f110fa1305
rhaskia <rhaskiagamedev@gmail.com>
2024-07-15 15:55:30 +1200

    feat: palette close on click off

--- gui/src/menu/palette.rs
+++ gui/src/menu/palette.rs
@@ -1,4 +1,7 @@
+use config::TerminalAction;
 use dioxus::prelude::*;
+
+use crate::{COMMAND_PALETTE, handle_action};
 
 #[component]
 pub fn CommandPalette() -> Element {
@@ -17,12 +20,38 @@ pub fn CommandPalette() -> Element {
     });
     let mut selected = use_signal(|| 0);
 
+    use_future(|| async {
+        wait_for_next_render().await;
+
+        let mut clickoff = eval(r#"
+            document.addEventListener('click', function(event) {
+                const divElement = document.getElementById('commandpalette');
+                if (divElement && !divElement.contains(event.target)) {
+                    dioxus.send({});
+                }
+            });
+        "#);
+
+        loop {
+            clickoff.recv().await;
+            handle_action(TerminalAction::ToggleCommandPalette);
+        }
+    });
+
     rsx! {
         div {
             class: "commandpalette",
+            id: "commandpalette",
             input {
                 class: "commandsearch",
                 oninput: move |event| search.set(event.value()),
+                onkeydown: move |e| match e.key() {
+                    Key::ArrowUp if selected() != 0 => selected -= 1, 
+                    Key::ArrowUp => *selected.write() = matches.read().len() - 1, 
+                    Key::ArrowDown if selected() != matches.read().len() - 1 => selected += 1, 
+                    Key::ArrowDown => *selected.write() = 0, 
+                    _ => {}
+                }
             }
             div {
                 class: "results",
