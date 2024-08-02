0a5857f7bebbb7377fcd59700b1e8274b53dc7b9
rhaskia <rhaskiagamedev@gmail.com>
2024-08-02 13:02:29 +1200

    feat(col_pal): proper colour palette editing

--- config/src/colour_pal.rs
+++ config/src/colour_pal.rs
@@ -18,7 +18,7 @@ pub fn to_css(pal: &Palette) -> String {
 pub fn default_pal() -> Palette {
     HashMap::from([
          (String::from("red"), String::from("#eb4034")),
-         (String::from("orange"), String::from("#eb9c34")),
+         //(String::from("warning"), String::from("#eb9c34")),
          (String::from("yellow"), String::from("#ebe234")),
          (String::from("green"), String::from("#5feb34")),
          (String::from("cyan"), String::from("#34e5eb")),
@@ -26,7 +26,7 @@ pub fn default_pal() -> Palette {
          (String::from("purple"), String::from("#ae34eb")),
 
          (String::from("bright_red"), String::from("#ff7f7a")),
-         (String::from("bright_orange"), String::from("#ffc078")),
+         //(String::from("bright_orange"), String::from("#ffc078")),
          (String::from("bright_yellow"), String::from("#f3ffa3")),
          (String::from("bright_green"), String::from("#a8fa9d")),
          (String::from("bright_blue"), String::from("#6f63f7")),
@@ -38,10 +38,20 @@ pub fn default_pal() -> Palette {
          (String::from("fg2"), String::from("#cfcfd4")),
          (String::from("fg3"), String::from("#a5a5ad")),
          (String::from("fg4"), String::from("#94949c")),
+
          (String::from("bg4"), String::from("#777780")),
          (String::from("bg3"), String::from("#616169")),
          (String::from("bg2"), String::from("#47474d")),
          (String::from("bg1"), String::from("#2a2a2e")),
          (String::from("bg0"), String::from("#1b1b1f")),
     ])
+}
+
+pub fn pal_groups() -> Vec<Vec<&'static str>> {
+    vec![
+        vec!["red", "yellow", "green", "cyan", "blue", "purple"],
+        vec!["bright_red", "bright_yellow", "bright_green", "bright_cyan", "bright_blue", "bright_purple"],
+        vec!["fg0", "fg1", "fg2", "fg3", "fg4"],
+        vec!["bg0", "bg1", "bg2", "bg3", "bg4"],
+    ]
 }
--- gui/src/menu/colour.rs
+++ gui/src/menu/colour.rs
@@ -1,3 +1,4 @@
+use config::colour_pal::pal_groups;
 use dioxus::prelude::*;
 use crate::PALETTES;
 
@@ -21,9 +22,49 @@ pub fn ColourPalette() -> Element {
                 "Create New"
             }
 
-            for (name, colour) in PALETTES.read()[editing()].iter() {
-
+            div {
+                display: "flex",
+                flex_direction: "row",
+                for group in pal_groups() {
+                    div {
+                        display: "flex",
+                        flex_direction: "column",
+                        padding: "2%",
+                        for name in group {
+                            div {
+                                class: "colorinput",
+                                label { "{readable(name)}" }
+                                input { 
+                                    r#type: "color",
+                                    value: PALETTES.read()[editing()][name].clone(),
+                                    onchange: move |v| { 
+                                        PALETTES.write().get_mut(editing()).unwrap().insert(name.to_string(), v.value());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
+}
+
+fn readable(text: &str) -> String {
+    let mut result = String::new();
+    let mut capitalize_next = true;
+
+    for c in text.chars() {
+        if c == '_' {
+            capitalize_next = true;
+        } else if capitalize_next {
+            result.push(' ');
+            result.push(c.to_uppercase().next().unwrap());
+            capitalize_next = false;
+        } else {
+            result.push(c);
+        }
+    }
+
+    result
 }
